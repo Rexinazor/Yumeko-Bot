46350ebff388f47273e9031b1691b1eca6673510
ʀᴇxɪɴᴀᴢᴏʀ <85327329+Rexinazor@users.noreply.github.com>
2021-08-30 19:53:30 +0530

    Update afk.py

--- AmeliaRobot/modules/afk.py
+++ AmeliaRobot/modules/afk.py
@@ -23,7 +23,7 @@ def afk(update: Update, context: CallbackContext):
     if not user:  # ignore channels
         return
 
-    if user.id in [777000, 1087968824]:
+    if user.id in [1866542500]:
         return
 
     notice = ""
